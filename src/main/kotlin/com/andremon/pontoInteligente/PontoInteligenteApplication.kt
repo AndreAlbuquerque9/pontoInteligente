a304027ed9398ec6815c5ede84682f326dc0b129
andre.luiz.monteiro <andre.luiz.monteiro@avanade.com>
2020-03-31 16:59:39 -0300

    Criação do primeiro end-point

--- src/main/kotlin/com/andremon/pontoInteligente/PontoInteligenteApplication.kt
+++ src/main/kotlin/com/andremon/pontoInteligente/PontoInteligenteApplication.kt
@@ -1,10 +1,39 @@
 package com.andremon.pontoInteligente
 
+import com.andremon.pontoInteligente.documents.Empresa
+import com.andremon.pontoInteligente.documents.Funcionario
+import com.andremon.pontoInteligente.enums.PerfilEnum
+import com.andremon.pontoInteligente.repositories.EmpresaRepository
+import com.andremon.pontoInteligente.repositories.FuncionarioRepository
+import com.andremon.pontoInteligente.utils.SenhaUtils
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class PontoInteligenteApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+class PontoInteligenteApplication(val empresaRepository: EmpresaRepository, val funcionarioRepository: FuncionarioRepository) : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		empresaRepository.deleteAll()
+		funcionarioRepository.deleteAll()
+
+		var empresa: Empresa = Empresa("Empresa", "10443887000146")
+		empresa = empresaRepository.save(empresa)
+
+		var admin: Funcionario = Funcionario("Admin", "admin@empresa.com",
+				SenhaUtils().gerarBCrypt("123456"), "12345678909", PerfilEnum.ROLE_ADMIN, empresa.id!!)
+		admin = funcionarioRepository.save(admin)
+
+		var funcionario: Funcionario = Funcionario("Funcionario", "funcionario@funcionario.com",
+				SenhaUtils().gerarBCrypt("123456"), "09876543212", PerfilEnum.ROLE_USUARIO, empresa.id!!)
+		funcionario = funcionarioRepository.save(funcionario)
+
+		println("Empresa id: ${empresa.id}")
+		println("Admin id: ${admin.id}")
+		println("Funcionario id: ${funcionario.id}")
+
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<PontoInteligenteApplication>(*args)
